c980966e5f10c5284bf1fa0a5feb34f83c958758
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2023-03-23 21:33:39 -0300

    new apy key

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = {
+    env: {
+        NASA_KEY: "kHwgPFH6S709YhS2hsgVJL2jcOSuGULZbiDNEyYk",
+    },
+    nextConfig,
+};
