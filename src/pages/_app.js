cbba89b69cd9365975062313c9a0c1ef0680219b
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2023-03-27 20:16:32 -0300

    vercel analytics add

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,16 @@
 import ContainerApp from "@/components/containerApp";
 import "../styles/reset.scss";
-
+import { Analytics } from "@vercel/analytics/react";
 import { Lato } from "@next/font/google";
 const lato = Lato({ weight: ["400", "700"], subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
     return (
-        <ContainerApp className={lato.className}>
-            <Component {...pageProps} />
-        </ContainerApp>
+        <>
+            <ContainerApp className={lato.className}>
+                <Component {...pageProps} />
+            </ContainerApp>
+            <Analytics />
+        </>
     );
 }
